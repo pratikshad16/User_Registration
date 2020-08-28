0222542a849f9d83cb31343cd0a7d68d54f40b58
pratikshad16 <dhagepratiksha16@gmail.com>
2020-08-29 01:38:16 +0530

    Added: Functionality for first name validation

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    private static final String NAME_PATTERN = "^[A-Z][a-z]{3,}$";
+    public boolean validateFirstName(String firstName)
+    {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(firstName).matches();
+
+    }
+}
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,4 +1,21 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.validateFirstName("Pratiksha");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.validateFirstName("Pi");
+        Assert.assertFalse(result);
+    }
+
 }
