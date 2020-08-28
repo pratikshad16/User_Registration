d6755998c0e60d6401cf9999b2f475e373a8e7fd
pratikshad16 <dhagepratiksha16@gmail.com>
2020-08-29 01:53:53 +0530

    Added: Functionality for last name validation

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -10,4 +10,10 @@ public class UserRegistration {
         return pattern.matcher(firstName).matches();
 
     }
+    public boolean validateLastName(String lastName)
+    {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(lastName).matches();
+
+    }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -18,4 +18,18 @@ public class UserRegistrationTest {
         Assert.assertFalse(result);
     }
 
+    @Test
+    public void givenLasttName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.validateFirstName("Dhage");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void name() {
+    }public void givenLasttName_WhenShort_ShouldReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.validateFirstName("Dh");
+        Assert.assertFalse(result);
+    }
 }
